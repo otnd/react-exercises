55db8c61f7980a2f3b44fc4dcdbdd33184a9d592
otnd <davide.morrone01@gmail.com>
2022-10-04 18:04:51 +0200

    forms-04

---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export class Login extends React.Component {
+    state = {
+        user: '',
+        password: '',
+        remember: ''
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        })
+    }
+
+    reset = () => {
+        this.setState({
+            user: '',
+            password: '',
+            remember: ''
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input name="user" type="text" value={this.state.user} onChange={this.handleInputChange} />
+                <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
+                <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
+                <button disabled={this.state.user && this.state.password ? false : true}>Login</button>
+                <button onClick={this.reset}>Reset</button>
+            </div>
+        )
+    }
+}
