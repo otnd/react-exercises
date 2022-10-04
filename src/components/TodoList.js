1a199e3d3656aeeacfb2d890daa83b7494d337c7
otnd <davide.morrone01@gmail.com>
2022-10-04 18:22:22 +0200

    lists-06

---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+    state = {
+        items: ['study', 'code', 'videogames'],
+        task: ''
+    }
+
+    handleChange = (e) => this.setState({ task: e.target.value });
+    handleClick = () => this.setState(state => ({ items: [...state.items, state.task], task: '' }));
+    handleReset = () => this.setState({ items: [], task: '' })
+    deleteTask = (e) => this.setState(state => {
+        const stateCopy = [...state.items];
+        stateCopy.splice(e.target.value, 1);
+        return { items: stateCopy };
+    })
+
+    render() {
+        return (
+            <>
+                <ul>{this.state.items.map((item, index) => <li key={index}>{item} <button value={index} onClick={this.deleteTask}>Delete</button></li>)}</ul>
+                <input type="text" name="task" value={this.state.task} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>Add task</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </>
+        )
+    }
+}
