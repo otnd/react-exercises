21a7fdbcceaa0e003898399096f137a191a6586d
otnd <davide.morrone01@gmail.com>
2022-10-04 17:34:50 +0200

    component-lifecycle-01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Counter from "./components/Counter";
+
 function App() {
   return (
-    
+    <Counter initialValue={1} incrementInterval={1000} incrementAmount={1} />
   );
 }
 
---
+++ src/components/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
+
+export class Counter extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { count: this.props.initialValue };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState((state) => ({ count: state.count + this.props.incrementAmount }))
+        }, this.props.incrementInterval)
+    }
+
+    render() {
+        return <CounterDisplay count={this.state.count} />
+    }
+}
+
+export default Counter;
