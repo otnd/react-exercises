14fa6968411b0bfaa1b0d5092e88d0768ea83246
otnd <davide.morrone01@gmail.com>
2022-10-04 16:48:45 +0200

    components-07

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    
-  );
+import Hello from './components/Hello';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+    <>
+      <Hello/>
+    </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Message from './Message';
+class Hello extends React.Component {
+    render() {
+        return (
+        <>
+            <h1>Hello, World!</h1>
+            <Message />
+        </>
+        );
+    }
+}
+
+export default Hello;
