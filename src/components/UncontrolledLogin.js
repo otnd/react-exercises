2529adb685d05f0fddb18b772a5ae9fee26c0baa
otnd <davide.morrone01@gmail.com>
2022-10-04 18:09:24 +0200

    forms-06

---
+++ src/components/UncontrolledLogin.js
@@ -0,0 +1,33 @@
+import React, { createRef } from "react";
+
+export class UncontrolledLogin extends React.Component {
+
+    handleChange = () => {
+        const user = this._formRef.current.elements.user.value;
+        const password = this._formRef.current.elements.password.value;
+        console.log(user, password)
+        if (user && password) {
+            this._formRef.current.elements.loginBtn.disabled = false
+        }
+        else {
+            this._formRef.current.elements.loginBtn.disabled = true
+        }
+    }
+
+    _formRef = createRef()
+
+    render() {
+        return (
+            <div>
+                <h1>Uncontrolled Form</h1>
+                <form ref={this._formRef} onChange={this.handleChange}>
+                    <input type="text" name="user" autoFocus />
+                    <input type="password" name="password" />
+                    <input type="checkbox" />
+                    <button name="loginBtn" disabled>Login</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </div>
+        )
+    }
+}
