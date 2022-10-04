227cc2a5391675425dfa421d658911a12f655627
otnd <davide.morrone01@gmail.com>
2022-10-04 16:52:07 +0200

    props-01

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <>
+                {<h1>Welcome, {this.props.name}!</h1>}
+            </>
+        )
+    }
+}
+export default Welcome;
