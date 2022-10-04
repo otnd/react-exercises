23cf7d6245fe7023e0c2a7b52e9443d284c470fa
otnd <davide.morrone01@gmail.com>
2022-10-04 18:45:48 +0200

    render-props-01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { TodoList } from "./components/TodoList";
+
 function App() {
   return (
-    
+    <TodoList render={
+      (state, fn) => state.items.map((item, index) => <li key={index}>{item} <button value={index} onClick={fn}>Delete</button></li>)
+    } />
   );
 }
 
