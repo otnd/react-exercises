f2a859745a7e438e98a189b88fb9788913362a47
otnd <davide.morrone01@gmail.com>
2022-10-05 22:45:44 +0200

    react-router-02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ClickCounter } from './components/ClickCounter'
+
 function App() {
   return (
-    
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/counter' element={<ClickCounter initialValue={1} increment={1} />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
+  )
 }
 
-export default App;
+export default App
