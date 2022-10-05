a5706336b776c8479b49f0c425f3e42a743c1c98
otnd <davide.morrone01@gmail.com>
2022-10-05 22:05:30 +0200

    usestate-02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Login from "./components/Login";
+
 function App() {
   return (
-    
+    <Login/>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Login(props) {
+    const [loginData, setData] = useState({
+        user: '',
+        password: '',
+        remember: false
+    })
+
+    function handleInputChange(e) {
+        setData(data => ({
+            ...data,
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
+        }))
+    }
+
+    return (
+        <>
+            <form action="">
+                <input type="text" name="user" placeholder="Username" value={loginData.user} onChange={handleInputChange} />
+                <input type="password" name="password" placeholder="Password" value={loginData.password} onChange={handleInputChange} />
+                <input type="checkbox" name="remember" checked={loginData.remember} onChange={handleInputChange} />
+                <button disabled={loginData.user && loginData.password ? false : true}>Login</button>
+            </form>
+        </>
+    )
+}
+
+export default Login
