451d8e3c4f9c7d7ee134b9655d49acdf8b1db93d
otnd <davide.morrone01@gmail.com>
2022-10-05 22:42:33 +0200

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import  Welcome from './components/Welcome'
+
 function App() {
   return (
-    
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Welcome name="Davide" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
+  )
 }
 
-export default App;
+export default App
