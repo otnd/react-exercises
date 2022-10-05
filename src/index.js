0d7a637244ae7aec3e7ecdd9e2250712e67fa74e
otnd <davide.morrone01@gmail.com>
2022-10-05 23:30:31 +0200

    redux-02

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { CounterState } from "./state/CounterState";
+import { combineReducers } from "redux";
+import { legacy_createStore as createStore } from "redux";
+
+const rootReducer = combineReducers({
+    counter: CounterState.reducer
+})
+
+const store = createStore(rootReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(CounterState.actions.increment(1))
+store.dispatch(CounterState.actions.decrement(2))
+store.dispatch(CounterState.actions.reset())
