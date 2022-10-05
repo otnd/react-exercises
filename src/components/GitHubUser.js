694e3abf516ed909035e3ab977274a5d5d05b2f3
otnd <davide.morrone01@gmail.com>
2022-10-05 23:21:40 +0200

    swr-01

---
+++ src/components/GitHubUser.js
@@ -0,0 +1,32 @@
+import useSWR from "swr"
+
+const fetcher = (url) =>
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(('An error occurred while fetching the data.'))
+            }
+            else return response.json()
+        })
+
+
+export function useGitHubUser(username) {
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+
+    return {
+        data, error, loading: !data && !error
+    }
+}
+
+
+export function GitHubUser({ username }) {
+    const { data, error, loading } = useGitHubUser(username)
+
+    return (
+        <>
+            {loading && <li>Loading...</li>}
+            {error && <li>An error has occurred</li>}
+            {data && <li>This user exist - {data.login}</li>}
+        </>
+    )
+}
